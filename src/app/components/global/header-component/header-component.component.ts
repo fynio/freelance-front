a3bb5a309105ffb4069db51a9214270553854839
rodrigo <42428236+fynio@users.noreply.github.com>
2020-11-10 12:07:36 -0600

    comenzamos

--- src/app/components/global/header-component/header-component.component.ts
+++ src/app/components/global/header-component/header-component.component.ts
@@ -7,7 +7,31 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HeaderComponentComponent implements OnInit {
 
-  constructor() { }
+  flagToggleMenu: boolean;
+
+  constructor() {
+    this.flagToggleMenu = false;
+  }
+
+
+  toogleMenu(): void {
+
+    const headerComponent = document.getElementById('header_contenedor');
+    const btnMenu = document.getElementById('btn_menu');
+
+    this.flagToggleMenu = !this.flagToggleMenu;
+    if (this.flagToggleMenu === true)
+    {
+      btnMenu.innerHTML = 'Cerrar';
+      headerComponent.style.marginLeft = '0px';
+    } else {
+      headerComponent.style.marginLeft = '120vw';
+      btnMenu.innerHTML = 'Menú';
+    }
+
+
+  }
+
 
   ngOnInit(): void {
   }
--- src/app/index/index.component.ts
+++ src/app/index/index.component.ts
@@ -7,7 +7,29 @@ import { Component, OnInit } from '@angular/core';
 })
 export class IndexComponent implements OnInit {
 
-  constructor() { }
+  datos: any;
+
+  constructor() {
+
+    this.datos = {
+      datos: [{
+        icono: './assets/image/blog.svg',
+        titulo: 'Publicar un trabajo',
+        texto: 'Es fácil. Simplemente publica un trabajo que necesitas realizar y recibe ofertas competitivas de freelancers en cuestión de minutos.',
+
+      }, {
+        icono: './assets/image/seleccionar.svg',
+        titulo: 'Elige freelancers',
+        texto: 'Independientemente de tus necesidades, existirá un freelancer que lo haga: desde diseño web, desarrolladores de app móviles, asistentes virtuales, fabricación de productos y diseño gráfico (y mucho más).',
+
+      }, {
+        icono: './assets/image/metododepago.svg',
+        titulo: 'Paga de manera segura',
+        texto: 'Con pagos seguros y miles de profesionales evaluados entre los cuales elegir, Freelancer.com es la manera más simple y segura de hacer trabajo en línea.',
+      }]
+    };
+
+  }
 
   ngOnInit(): void {
   }
